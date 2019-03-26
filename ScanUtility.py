5f51a15f078f18a1ee3daa7d7c54f9b9e627a85c
singaCapital <45968638+singaCapital@users.noreply.github.com>
2019-03-26 14:35:52 +0000

    Add files via upload

--- ScanUtility.py
+++ ScanUtility.py
@@ -1,14 +1,22 @@
 #This is a working prototype. DO NOT USE IT IN LIVE PROJECTS
 
+
 import sys
 import struct
 import bluetooth._bluetooth as bluez
 
+OGF_LE_CTL=0x08
+OCF_LE_SET_SCAN_ENABLE=0x000C
+
 def hci_enable_le_scan(sock):
     hci_toggle_le_scan(sock, 0x01)
 
+def hci_disable_le_scan(sock):
+    hci_toggle_le_scan(sock, 0x00)
+
 def hci_toggle_le_scan(sock, enable):
     cmd_pkt = struct.pack("<BB", enable, 0x00)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, cmd_pkt)
 
 def packetToString(packet):
     """
@@ -32,13 +40,59 @@ def parse_events(sock, loop_count=100):
         packetOffset = 0
         dataString = packetToString(packet)
         """
-        If the bluetooth device is an iBeacon then show the beacon.
+        If the bluetooth device is an beacon then show the beacon.
         """
+        #print (dataString)
+        if dataString[34:50] == '0303aafe1516aafe' or '0303AAFE1116AAFE':
+            """
+            Selects parts of the bluetooth packets.
+            """
+            broadcastType = dataString[50:52]
+            if broadcastType == '00' :
+                type = "Eddystone UID"
+                namespace = dataString[54:74].upper()
+                instance = dataString[74:86].upper()
+                resultsArray = [
+                {"type": type, "namespace": namespace, "instance": instance}]
+                return resultsArray
+
+            elif broadcastType == '10':
+                type = "Eddystone URL"
+                urlprefix = dataString[54:56]
+                if urlprefix == '00':
+                    prefix = 'http://www.'
+                elif urlprefix == '01':
+                    prefix = 'https://www.'
+                elif urlprefix == '02':
+                    prefix = 'http://'
+                elif urlprefix == '03':
+                    prefix = 'https://'
+                hexUrl = dataString[56:][:-2]
+                url = prefix + hexUrl.decode("hex")
+                rssi, = struct.unpack("b", packet[packetOffset -1])
+                resultsArray = [{"type": type, "url": url}]
+                return resultsArray
+
+            elif broadcastType == '20':
+                type = "Eddystone TLM"
+                resultsArray = [{"type": type}]
+                return resultsArray
+
+            elif broadcastType == '30':
+                type = "Eddystone EID"
+                resultsArray = [{"type": type}]
+                return resultsArray
+
+            elif broadcastType == '40':
+                type = "Eddystone RESERVED"
+                resultsArray = [{"type": type}]
+                return resultsArray
+
         if dataString[38:46] == '4c000215':
             """
             Selects parts of the bluetooth packets.
             """
-            #print (dataString)
+            type = "iBeacon"
             uuid = dataString[46:54] + "-" + dataString[54:58] + "-" + dataString[58:62] + "-" + dataString[62:66] + "-" + dataString[66:78]
             major = dataString[78:82]
             minor = dataString[82:86]
@@ -52,14 +106,7 @@ def parse_events(sock, loop_count=100):
             macAddress = ':'.join(a+b for a,b in zip(fixStructure, fixStructure))
             rssi, = struct.unpack("b", packet[packetOffset -1])
 
-            #print("UUID",uuid)
-            #print("Major",majorVal)
-            #print("Minor",minorVal)
-            #print("Mac Address",macAddress)
-            #print ("RSSI:", rssi)
-            #print ("----------")
-
-            resultsArray = [{"uuid": uuid, "major": majorVal, "minor": minorVal, "rssi": rssi, "macAddress": macAddress}]
+            resultsArray = [{"type": type, "uuid": uuid, "major": majorVal, "minor": minorVal, "rssi": rssi, "macAddress": macAddress}]
 
             return resultsArray
 
