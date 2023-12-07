fa68c104ffb03fc95f4c1fee5179367d6961a04e
Vishal L <102230304+vishal03121998@users.noreply.github.com>
2023-12-07 12:55:56 +0530

    Create Scanutility.py
    Updated Eddystone TLM and EID

---
+++ Scanutility.py
@@ -0,0 +1,127 @@
+#Updated for Eddystone TLM and EID
+
+import sys
+import struct
+import bluetooth._bluetooth as bluez
+
+OGF_LE_CTL=0x08
+OCF_LE_SET_SCAN_ENABLE=0x000C
+
+def hci_enable_le_scan(sock):
+    hci_toggle_le_scan(sock, 0x01)
+
+def hci_disable_le_scan(sock):
+    hci_toggle_le_scan(sock, 0x00)
+
+def hci_toggle_le_scan(sock, enable):
+    cmd_pkt = struct.pack("<BB", enable, 0x00)
+    bluez.hci_send_cmd(sock, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE, cmd_pkt)
+
+def packetToString(packet):
+    """
+    Returns the string representation of a raw HCI packet.
+    """
+    if sys.version_info > (3, 0):
+        return ''.join('%02x' % struct.unpack("B", bytes([x]))[0] for x in packet)
+    else:
+        return ''.join('%02x' % struct.unpack("B", x)[0] for x in packet)
+
+def parse_events(sock, loop_count=100):
+    old_filter = sock.getsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, 14)
+    flt = bluez.hci_filter_new()
+    bluez.hci_filter_all_events(flt)
+    bluez.hci_filter_set_ptype(flt, bluez.HCI_EVENT_PKT)
+    sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, flt )
+    results = []
+    for i in range(0, loop_count):
+        packet = sock.recv(255)
+        ptype, event, plen = struct.unpack("BBB", packet[:3])
+        packetOffset = 0
+        dataString = packetToString(packet)
+        """
+        If the bluetooth device is an beacon then show the beacon.
+        """
+        #print (dataString)
+        if (dataString[34:42] == '0303aafe'):
+            """
+            Selects parts of the bluetooth packets.
+            """
+            broadcastType = dataString[50:52]
+            if broadcastType == '00' :
+                type = "Eddystone UID"
+                namespace = dataString[54:74].upper()
+                instance = dataString[74:86].upper()
+                resultsArray = [
+                {"type": type, "namespace": namespace, "instance": instance}]
+                return resultsArray
+
+            elif broadcastType == '10':
+                type = "Eddystone URL"
+                urlprefix = dataString[54:56]
+                if urlprefix == '00':
+                    prefix = 'http://www.'
+                elif urlprefix == '01':
+                    prefix = 'https://www.'
+                elif urlprefix == '02':
+                    prefix = 'http://'
+                elif urlprefix == '03':
+                    prefix = 'https://'
+                hexUrl = dataString[56:][:-2]
+                if sys.version_info[0] == 3:
+                    url = prefix + bytes.fromhex(hexUrl).decode('utf-8')
+                    rssi, = struct.unpack("b", bytes([packet[packetOffset-1]]))
+                else:
+                    url = prefix + hexUrl.decode("hex")
+                    rssi, = struct.unpack("b", packet[packetOffset-1])
+                resultsArray = [{"type": type, "url": url}]
+                return resultsArray
+
+            elif broadcastType == '20':
+                type = "Eddystone TLM"
+                voltage = int(dataString[54:58], 16)
+                temperature = int(dataString[58:62], 16)
+                adv_count = int(dataString[62:70], 16)
+                sec_count = int(dataString[70:78], 16)
+                resultsArray = [
+                    {"type": type, "voltage": voltage, "temperature": temperature, "adv_count": adv_count, "sec_count": sec_count}]
+                return resultsArray
+          
+            elif broadcastType == '30':
+                type = "Eddystone EID"
+                # Extract Ephemeral Identifier (EID) data as needed
+                # Example: eid_data = dataString[54:74]
+                # Update the resultsArray accordingly
+                resultsArray = [{"type": type, "eid_data": eid_data}]
+                return resultsArray
+
+            elif broadcastType == '40':
+                type = "Eddystone RESERVED"
+                resultsArray = [{"type": type}]
+                return resultsArray
+
+        if dataString[38:46] == '4c000215':
+            """
+            Selects parts of the bluetooth packets.
+            """
+            type = "iBeacon"
+            uuid = dataString[46:54] + "-" + dataString[54:58] + "-" + dataString[58:62] + "-" + dataString[62:66] + "-" + dataString[66:78]
+            major = dataString[78:82]
+            minor = dataString[82:86]
+            majorVal = int("".join(major.split()[::-1]), 16)
+            minorVal = int("".join(minor.split()[::-1]), 16)
+            """
+            Organises Mac Address to display properly
+            """
+            scrambledAddress = dataString[14:26]
+            fixStructure = iter("".join(reversed([scrambledAddress[i:i+2] for i in range(0, len(scrambledAddress), 2)])))
+            macAddress = ':'.join(a+b for a,b in zip(fixStructure, fixStructure))
+            if sys.version_info[0] == 3:
+                rssi, = struct.unpack("b", bytes([packet[packetOffset-1]]))
+            else:
+                rssi, = struct.unpack("b", packet[packetOffset-1])
+
+            resultsArray = [{"type": type, "uuid": uuid, "major": majorVal, "minor": minorVal, "rssi": rssi, "macAddress": macAddress}]
+
+            return resultsArray
+
+    return results
